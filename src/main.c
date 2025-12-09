e17d31096b6e932be64d893a7c3df5c6c3724b99
WacKEDmaN <35508035+WacKEDmaN@users.noreply.github.com>
2025-12-09 19:47:55 +1100

    Add files via upload
    depreciated sys_clock fix

--- src/main.c
+++ src/main.c
@@ -9,6 +9,7 @@
 #include "vga_pio.h"
 #include "cpc_pio.h"
 #include "constants.h"
+#include "hardware/clocks.h"
 
 
 // Just a copy of amstrad screen. Each byte corresponds to one pixel (mode 2). Only 6 bits are used - 2 for each rgb color.
@@ -21,7 +22,18 @@ int main() {
     // Because of that, system clock should be divisible by CPC pixel clock.
     // CPC pixel clock in mode 2 is 16Mhz. When we set system clock to 128Mhz (instead of standard 125Mhz),
     // we can read one pixel in exactly 8 cycles.
-    set_sys_clock_khz(128000, true);
+	
+	
+    // old way (removed on RP2350):
+    // set_sys_clock_khz(133000, true);
+
+    // new way (works on both RP2040 and RP2350):
+    clock_configure(clk_sys,
+                    CLOCKS_CLK_SYS_CTRL_SRC_VALUE_CLKSRC_CLK_SYS_AUX,
+                    0,
+                    128 * MHZ,
+                    128 * MHZ);
+    // plus any PLL setup you need, etc.
 
     setup_vfreq_and_switch();
     fill_test_screen(screen);
